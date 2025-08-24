f95aa1473d4ce8636115e111c3600db356ab0d5c
ErickRezende1833 <erick.rez.contato@gmail.com>
2025-08-24 15:21:05 -0300

    Branch erick (#2)
    * classebasicas + atributosclientes
    
    * contrutor item
    
    * contrutoritem
    
    ---------
    
    Co-authored-by: Erick <persona0rcc@gmail.com>

--- src/ItemCardapio.java
+++ src/ItemCardapio.java
@@ -1,2 +1,13 @@
+
 public class ItemCardapio {
+    String nomeItem;
+    int idItem;
+    double preco;
+
+    public ItemCardapio(String nomeItem, int idItem, double preco){
+        this.nomeItem = nomeItem;
+        this.idItem = idItem;
+        this.preco = preco;
+    }
+
 }
