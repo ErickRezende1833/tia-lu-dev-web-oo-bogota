1170337ca5bb5eff4d40491a0511767d24548dd7
ErickRezende1833 <erick.rez.contato@gmail.com>
2025-08-24 16:28:48 -0300

    Branch erick (#3)
    * classebasicas + atributosclientes
    
    * contrutor item
    
    * contrutoritem
    
    * menu basico
    
    * menu um pouco melhor
    
    * menu um pouco melhor
    
    ---------
    
    Co-authored-by: Erick <persona0rcc@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,42 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        while (true){
+
+            int opcao = mostrarMenu(scanner);
+
+            switch (opcao){
+                case 1:
+                    System.out.println("Menu Clientes selecionado");
+                    break;
+                case 2:
+                    System.out.println("Menu Cardápio selecionado");
+                    break;
+                case 3:
+                    System.out.println("Menu pedidos selecionado");
+                    break;
+                default:
+                    System.out.println("Opção invalida");
+                    break;
+
+            }
+        }
     }
+
+    public static int mostrarMenu(Scanner scanner){
+
+        System.out.println("Menu");
+        System.out.println("1 = Menu Clientes");
+        System.out.println("2 = Menu Cardápio");
+        System.out.println("3 = Menu Pedidos");
+
+        return scanner.nextInt();
+    }
+
+
 }
