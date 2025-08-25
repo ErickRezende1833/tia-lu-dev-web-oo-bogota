937cae6c4042e128f4a1616f500b3558ad7bc3ef
Laysa Liima <142900878+LaysaLiima@users.noreply.github.com>
2025-08-25 17:37:08 -0300

    fix(clienteService): instancia novo cliente

--- src/servicos/ClienteService.java
+++ src/servicos/ClienteService.java
@@ -1,10 +1,12 @@
 package servicos;
 
 import dados.BancoDeDados;
+import modelos.Cliente;
 
 public class ClienteService {
     
     public static void cadastrarCliente(String nome, String telefone) {
         int id = BancoDeDados.gerarIdCliente();
+        Cliente novoCliente = new Cliente(id, nome, telefone);
     }
 }
