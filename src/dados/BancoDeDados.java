beb43be556679a589861d3648eaf5308c535d357
Laysa Liima <142900878+LaysaLiima@users.noreply.github.com>
2025-08-25 16:57:26 -0300

    feat(banco): adiciona métodos para gerar IDs de cliente, item e pedido

--- src/dados/BancoDeDados.java
+++ src/dados/BancoDeDados.java
@@ -10,7 +10,12 @@ public class BancoDeDados {
     public static ArrayList<Item> itens = new ArrayList<>();
     public static ArrayList<Pedido> pedidos = new ArrayList<>();
     
+
     private static int nextClienteId = 1;
     private static int nextItemId = 1;
     private static int nextPedidoId = 1;
+
+    public static int gerarIdCliente() { return nextClienteId++; }
+    public static int gerarIdItem() { return nextItemId++; }
+    public static int gerarIdPedido() { return nextPedidoId++; }
     }
