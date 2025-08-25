d3e5992261d72a98ba15ca8f800e51225eda1f7b
Laysa Liima <142900878+LaysaLiima@users.noreply.github.com>
2025-08-25 17:01:10 -0300

    feat(banco): implementa método inicializarMock com clientes e itens

--- src/dados/BancoDeDados.java
+++ src/dados/BancoDeDados.java
@@ -18,4 +18,14 @@ public class BancoDeDados {
     public static int gerarIdCliente() { return nextClienteId++; }
     public static int gerarIdItem() { return nextItemId++; }
     public static int gerarIdPedido() { return nextPedidoId++; }
+
+    public static void inicializarMock() {
+        clientes.add(new Cliente(gerarIdCliente(), "Erick Rezende", "99999-1111"));
+        clientes.add(new Cliente(gerarIdCliente(), "Laysa Lima", "98888-2222"));
+        clientes.add(new Cliente(gerarIdCliente(), "Vinicios Costa", "97777-3333"));
+
+        itens.add(new Item(gerarIdItem(), "Bolo de chocolate", "Sobremesa", 35.90));
+        itens.add(new Item(gerarIdItem(), "Hambúrguer", "Comida", 22.50));
+        itens.add(new Item(gerarIdItem(), "Coca-Cola 2L", "Bebida", 10.00));
     }
+}
