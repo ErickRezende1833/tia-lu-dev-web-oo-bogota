c58684281aae00a7cd7668d3cc4b8fe8beb58a12
Laysa Liima <142900878+LaysaLiima@users.noreply.github.com>
2025-08-25 17:53:36 -0300

    fix(itemService): adiciona geração de ID para novos itens

--- src/servicos/ItemService.java
+++ src/servicos/ItemService.java
@@ -1,7 +1,11 @@
 package servicos;
 
+import dados.BancoDeDados;
+
 public class ItemService {
 	
+    
     public static void cadastrarItem(String nome, String tipo, double preco) {
+        int id = BancoDeDados.gerarIdItem();
     }
 }   
