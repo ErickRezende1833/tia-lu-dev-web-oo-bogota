b4c36a89097f650f8e999a299f8c7ad70790bc82
Laysa Liima <142900878+LaysaLiima@users.noreply.github.com>
2025-08-25 16:46:25 -0300

    feat(banco): adiciona listas para clientes, itens e pedidos

--- src/dados/BancoDeDados.java
+++ src/dados/BancoDeDados.java
@@ -1,4 +1,12 @@
 package dados;
 
-public class BancoDeDados {   
-}
+import java.util.ArrayList;
+import modelos.Cliente;
+import modelos.Item;
+import modelos.Pedido;
+
+public class BancoDeDados {
+    public static ArrayList<Cliente> clientes = new ArrayList<>();
+    public static ArrayList<Item> itens = new ArrayList<>();
+    public static ArrayList<Pedido> pedidos = new ArrayList<>();
+    }
