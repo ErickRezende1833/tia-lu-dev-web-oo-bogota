9e6239273c3069249056c52fb911b9cd5c02e6c0
Laysa Liima <142900878+LaysaLiima@users.noreply.github.com>
2025-08-25 17:35:31 -0300

    fix(clienteService): adiciona geração de ID para novos clientes

--- src/servicos/ClienteService.java
+++ src/servicos/ClienteService.java
@@ -1,7 +1,10 @@
 package servicos;
 
+import dados.BancoDeDados;
+
 public class ClienteService {
     
     public static void cadastrarCliente(String nome, String telefone) {
+        int id = BancoDeDados.gerarIdCliente();
     }
 }
