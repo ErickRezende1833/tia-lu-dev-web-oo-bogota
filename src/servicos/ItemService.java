6d70d69d0df87d4f48b5b60c3db70e1c25cff212
Laysa Liima <142900878+LaysaLiima@users.noreply.github.com>
2025-08-25 17:57:05 -0300

    fix(itemService): instancia novo item

--- src/servicos/ItemService.java
+++ src/servicos/ItemService.java
@@ -1,11 +1,12 @@
 package servicos;
 
 import dados.BancoDeDados;
+import modelos.Item;
 
 public class ItemService {
-	
     
     public static void cadastrarItem(String nome, String tipo, double preco) {
         int id = BancoDeDados.gerarIdItem();
+        Item novoItem = new Item(id, nome, tipo, preco);
     }
 }   
