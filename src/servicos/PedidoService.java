efdb450f021549febbad0343921442a98559ec61
Laysa Liima <142900878+LaysaLiima@users.noreply.github.com>
2025-08-26 08:55:31 -0300

    feat(pedidoService): lê ID do cliente informado pelo usuário

--- src/servicos/PedidoService.java
+++ src/servicos/PedidoService.java
@@ -5,5 +5,8 @@ import java.util.Scanner;
 public class PedidoService { 
 	public static void criarPedido(Scanner sc) { 
 		System.out.print("Digite o ID do cliente: "); 
-	}
+	
+	int idCliente = sc.nextInt(); 
+	
+		} 
 }
