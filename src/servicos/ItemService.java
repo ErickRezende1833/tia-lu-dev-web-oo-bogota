803563affe446b647fd4acf0b9798c055bc45f5e
Erick <persona0rcc@gmail.com>
2025-08-26 17:09:05 -0300

    feat (itemservice): metodo para cadastrar itens

--- src/servicos/ItemService.java
+++ src/servicos/ItemService.java
@@ -3,13 +3,20 @@ package servicos;
 import dados.BancoDeDados;
 import modelos.Item;
 
+import java.util.Scanner;
+
 public class ItemService {
-    
-    public static void cadastrarItem(String nome, String tipo, double preco) {
+
+    public static void cadastrarItem(Scanner sc){
+        System.out.print("Digite o nome do item: ");
+        String nome = sc.nextLine();
+        System.out.print("Digite o tipo do item: ");
+        String tipo = sc.nextLine();
+        System.out.print("Digite o preço do item: ");
+        double preco = sc.nextDouble();
         int id = BancoDeDados.gerarIdItem();
         Item novoItem = new Item(id, nome, tipo, preco);
-
-        BancoDeDados.itens.add(novoItem);   
+        BancoDeDados.itens.add(novoItem);
         System.out.println("Item cadastrado com sucesso! ID: " + id);
     }
     
