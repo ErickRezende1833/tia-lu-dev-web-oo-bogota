31d5e04849694a2d001c0c5767395ebad53d84d6
Erick <persona0rcc@gmail.com>
2025-08-26 17:05:37 -0300

    feat (clientesevice): medoto para cadastrar clientes

--- src/servicos/ClienteService.java
+++ src/servicos/ClienteService.java
@@ -3,12 +3,17 @@ package servicos;
 import dados.BancoDeDados;
 import modelos.Cliente;
 
+import java.util.Scanner;
+
 public class ClienteService {
-    
-    public static void cadastrarCliente(String nome, String telefone) {
+
+    public static void cadastrarCliente(Scanner sc){
+        System.out.print("Digite o nome do cliente: ");
+        String nome = sc.nextLine();
+        System.out.print("Digite o telefone do cliente: ");
+        String telefone = sc.nextLine();
         int id = BancoDeDados.gerarIdCliente();
         Cliente novoCliente = new Cliente(id, nome, telefone);
-
         BancoDeDados.clientes.add(novoCliente);
         System.out.println("Cliente cadastrado com sucesso! ID: " + id);
     }
