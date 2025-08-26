f06afadf78b950b239a8fb827cf7d72e4d42e74a
Laysa Liima <142900878+LaysaLiima@users.noreply.github.com>
2025-08-26 09:09:27 -0300

    feat(pedidoService): gera ID do pedido baseado no total atual de pedidos

--- src/servicos/PedidoService.java
+++ src/servicos/PedidoService.java
@@ -1,6 +1,7 @@
 package servicos; 
 
 import java.util.Scanner; 
+import dados.BancoDeDados; 
 
 public class PedidoService { 
 	public static void criarPedido(Scanner sc) { 
@@ -8,5 +9,6 @@ public class PedidoService {
 	int idCliente = sc.nextInt(); 
 		System.out.print("Digite o ID do item: "); 
 	int idItem = sc.nextInt(); 
+	int idPedido = BancoDeDados.pedidos.size() + 1; 
 		} 
 }
