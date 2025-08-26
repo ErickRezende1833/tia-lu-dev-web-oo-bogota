3e1cb29daed516a5baa828560fc9738b139dbd10
Laysa Liima <142900878+LaysaLiima@users.noreply.github.com>
2025-08-26 09:47:47 -0300

    feat(main): inicia estrutura de repetição para exibir menu

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,10 +2,17 @@ package app;
 
 import java.util.Scanner;
 import dados.BancoDeDados;
+import servicos.ClienteService;
+import servicos.ItemService;
+import servicos.PedidoService;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         BancoDeDados.inicializarMock();
+
+        int opcao;
+        do {
+          
     }
 }
