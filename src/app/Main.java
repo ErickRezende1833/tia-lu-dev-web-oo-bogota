786e3d88e7292e6d1640c1c94bfcbdd2e8e3f56f
Laysa Liima <142900878+LaysaLiima@users.noreply.github.com>
2025-08-26 09:39:21 -0300

    feat(main): inicia scanner para entrada de dados e carrega dados iniciais

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,6 +1,11 @@
 package app;
 
+import java.util.Scanner;
+import dados.BancoDeDados;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BancoDeDados.inicializarMock();
     }
 }
