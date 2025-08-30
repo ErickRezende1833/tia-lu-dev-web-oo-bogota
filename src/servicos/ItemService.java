70584e476955f9ff51ebd4be10ac265a7ff9d056
Erick <persona0rcc@gmail.com>
2025-08-30 20:51:08 -0300

    feat (itemservice): melhorando a logica de encontrar erros

--- src/servicos/ItemService.java
+++ src/servicos/ItemService.java
@@ -3,6 +3,7 @@ package servicos;
 import dados.BancoDeDados;
 import modelos.Item;
 
+import java.util.Objects;
 import java.util.Scanner;
 
 public class ItemService {
@@ -11,8 +12,16 @@ public class ItemService {
         try {
             System.out.print("Digite o nome do item: ");
             String nome = sc.nextLine();
+            if (Objects.equals(nome, "")){
+                System.out.println("Erro: nome invalido ");
+                cadastrarItem(sc);
+            }
             System.out.print("Digite o tipo do item: ");
             String tipo = sc.nextLine();
+            if (Objects.equals(nome, "")){
+                System.out.println("Erro: nome invalido ");
+                cadastrarItem(sc);
+            }
             System.out.print("Digite o preço do item: ");
             double preco = sc.nextDouble();
             int id = BancoDeDados.gerarIdItem();
